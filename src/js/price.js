1db55fc475240ecf21f705a4534e47d8e0b42bfa
Pavel Proyavin <pavel@dorohovdesign.com>
2019-06-10 11:05:33 +0300

    v1.1.0

--- src/js/price.js
+++ src/js/price.js
@@ -2,6 +2,8 @@
     "use strict"
     $(function() {
 
+        firstLoad()
+
         function showDropdown() {
             $('.repair--price__dropdown__container').addClass('is--active')
         }
@@ -18,16 +20,40 @@
             $('.__price-tab').removeClass('is--active')
         }
 
-        function showTab(id, title) {
+        function showTab(id) {
             hideTab()
-            $('.__price-tab#' + id).addClass('is--active')
-            $('.repair--price__dropdown__label').html(title)
+            var container = $('.__price-tab[data-target="' + id + '"]').addClass('is--active')
+            $('.repair--price__dropdown__label').html(container.data('title'))
+            getSt(id)
         }
 
-        $('.repair--price__dropdown__container ul li').on('click', function() {
-            showTab($(this).data('target'), $(this).children('span').html())
+        function firstLoad() {
+            if($('.repair--price__dropdown__container ul li').length >= 1) {
+                var firstHash = $('.repair--price__dropdown__container ul li')[0]
+                var hash = $(firstHash).children('a')[0].hash
+                showTab(hash)
+            }
+        }
+
+        function getSt(id) {
+            $('.repair--price__st').removeClass('is--active')
+            $('.repair--price__st[data-target="' + id + '"]').addClass('is--active')
+        }
+
+        $('a.--repair-link').on('click', function(e) {
+            e.preventDefault()
+            var hash = e.currentTarget.hash
+            showTab(hash)
             hideDropdown()
+            if($(this).hasClass('is--carousel')) {
+                $('html,body').animate({scrollTop: $('.repair--price#price').offset().top - $('.navbar').innerHeight()},'slow');
+            }
         })
+
+        // $('.repair--price__dropdown__container ul li').on('click', function() {
+        //     showTab($(this).data('target'), $(this).children('span').html())
+        //     hideDropdown()
+        // })
 
         $('.repair--price__dropdown__label').on('click', function() {
             toggleDropdown()
